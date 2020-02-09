3f4ada5adc5d270a70a9e8575e2341cd05ac39d2
EC2 Default User <ec2-user@ip-172-31-34-126.us-east-2.compute.internal>
2020-02-09 07:32:27 +0000

    update

--- guessNumber/js/function.js
+++ guessNumber/js/function.js
@@ -1,15 +1,12 @@
 
-
 var randomNumber  = Math.floor(Math.random() * 99) + 1; // global var generationg a random number between 1 and 99.99
+
 var guesses = document.querySelector('#guesses');
 var lastResult = document.querySelector('#lastResult');
 var lowOrHi = document.querySelector('#lowOrHi');
 
-var guessSubmit = document.querySelector('.guessSubmit');
+var guessSubmit = document.querySelector('.guessSubmit'); // guessSubmit button from html page 
 var guessField = document.querySelector('.guessField');
-
-
-
 
 var guessCount = 1; 
 var resetButton = document.querySelector('#reset')
@@ -18,14 +15,16 @@ resetButton.style.display = 'none';
 guessField.focus();
 var resetButton; 
 
-function checkGuess(){
-  var userGuess = Number(guessField.value);
+//************functions**************
+
+function checkGuess(){ // submit guesses in pressed 
+  var userGuess = Number(guessField.value); // Once a guess is submitted, record the guess so the player can see previous guesses
   if(guessCount ===1){
-    guesses.innerHTML = 'previous gueses: ';
+    guesses.innerHTML = 'Previous guesses: ';
   }
   guesses.innerHTML += userGuess + ' ';
-  
-    if(userGuess === randomNumber){
+    
+    if(userGuess === randomNumber){ // Next, determine if the number is correct ( If so, display a congratulations message. If not, display if the number is too high or too low
       lastResult.innerHTML = 'Congratulations! You got it right!';
       lastResult.style.backgroundColor = 'green';
       lowOrHi.innerHTML= '';
@@ -38,38 +37,54 @@ function checkGuess(){
         lastResult.style.backgroundColor ='red';
         if(userGuess < randomNumber){
           lowOrHi.innerHTML = 'Last guess was too low!';
-        }else if (userGuess > randomNumber){
-          lowOrHi.innerHTML = 'Last gueses was too high!';
+        }else if (userGuess > randomNumber && userGuess <= 99 ) {
+          lowOrHi.innerHTML = 'Last guess was too high!';
+        }else if (userGuess > 99){
+          lowOrHi.innerHTML = 'out of bound input';
+          guessCount = guessCount -1;
+        }else if(isNaN() === true){
+          lowOrHi.innerHTML = 'Not a number! invaild input';
+           guessCount = guessCount -1;
+          
         }
+        
       }
-    guessCount++;
-    guessField.value = '';
-    guessField.focus();
-}
-guessSubmit.addEventListener('click',checkGuess);
+    guessCount++; // add to count : Determine, how many turns the player has left
+  
+    guessField.value = ''; 
+    guessField.focus(); // refocus on the guessField
+    
+ }
+
+guessSubmit.addEventListener('click',checkGuess); // Event listener waits for button of guessSumit 
 
 function setGameOver(){
-  guessField.disabled = true; 
-  guessSubmit.disabled = true; 
-  resetButton.style.display = 'inline';
-  resetButton.addEventListener('click',resetGame);
+  guessField.disabled = true; //  disables the text input 
+  guessSubmit.disabled = true; //  disabeles the submit button
+  resetButton.style.display = ''; // display reset button 
+  resetButton.addEventListener('click',resetGame); // event listener for reset game. 
+ 
 }
 
-function resetGame(){
-  guessCount = 1; 
-  
-  var resetParas = document.querySelectorAll('.resultParas p');
+
+function resetGame() {
+ 
+  guessCount = 1; //guessCount back down to 1
+  console.log("resetGame function")
+  var resetParas = document.querySelectorAll('.resultParas p'); // reset text content to blank
   for(var i = 0; i < resetParas.length; i++){
     resetParas[i].textContent = '';
    }
-  resetButton.style.display = 'none';
+  resetButton.style.display = 'none'; // Hides the reset button
   
-  guessField.disabled = 'false';
+  guessField.disabled = 'false'; // Enables the form elements, and empties and focuses the text field
   guessSubmit.disabled = 'false';
-  guessField.value = '';
   guessField.focus();
+ 
+ 
+  lastResult.style.backgroundColor = 'white'; // Removes the background color 
   
-  lastResult.style.backgroundColor = 'white'; 
-  
-  randomNumber = Math.floor(Math.random() * 99) + 1;
+  randomNumber = Math.floor(Math.random() * 99) + 1; // Generates a new random number 
 }
+//************functions* end *************
+
